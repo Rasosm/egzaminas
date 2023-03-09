bc858634a0affefa815ba5747507cb20082dfc05
Rasosm <rasosm@gmail.com>
2023-03-09 13:07:27 +0200

    Style update

--- app/Providers/CartServiceProvider.php
+++ app/Providers/CartServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use App\Services\CartService;
+use Illuminate\Pagination\Paginator;
 
 class CartServiceProvider extends ServiceProvider
 {
